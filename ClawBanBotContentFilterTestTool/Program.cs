e9b656b71f9dcac607e12ac5981df97f2d15ddf0
Luce Carter <luce@lucyscarter.co.uk>
2020-12-18 16:47:55 +0000

    :tada: Initial Committ

---
+++ ClawBanBotContentFilterTestTool/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Microsoft.Azure.CognitiveServices.ContentModerator.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace ClawBanBotContentFilterTestTool
+{
+    class Program
+    {
+        // Your Content Moderator subscription key is found in your Azure portal resource on the 'Keys' page.
+        private static readonly string SubscriptionKey = "CONTENT_MODERATOR_SUBSCRIPTION_KEY";
+        // Base endpoint URL. Found on 'Overview' page in Azure resource. For example: https://westus.api.cognitive.microsoft.com
+        private static readonly string Endpoint = "CONTENT_MODERATOR_ENDPOINT";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClawBanBotContentFilterTestTool/Secrets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClawBanBotContentFilterTestTool
+{
+    public static class Secrets
+    {
+        // Your Content Moderator subscription key is found in your Azure portal resource on the 'Keys' page.
+        private static readonly string SubscriptionKey = "CONTENT_MODERATOR_SUBSCRIPTION_KEY";
+        // Base endpoint URL. Found on 'Overview' page in Azure resource. For example: https://westus.api.cognitive.microsoft.com
+        private static readonly string Endpoint = "CONTENT_MODERATOR_ENDPOINT";
+    }
+}
