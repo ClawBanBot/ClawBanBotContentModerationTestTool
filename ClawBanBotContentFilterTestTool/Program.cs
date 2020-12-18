46c5dab414ed946817c67f76715b89bce5443e0b
Luce Carter <luce@lucyscarter.co.uk>
2020-12-18 17:10:17 +0000

    :sparkles: Functioning app from Quickstart. Outputs to text file

--- ClawBanBotContentFilterTestTool/Program.cs
+++ ClawBanBotContentFilterTestTool/Program.cs
@@ -11,15 +11,70 @@ namespace ClawBanBotContentFilterTestTool
 {
     class Program
     {
-        // Your Content Moderator subscription key is found in your Azure portal resource on the 'Keys' page.
-        private static readonly string SubscriptionKey = "CONTENT_MODERATOR_SUBSCRIPTION_KEY";
-        // Base endpoint URL. Found on 'Overview' page in Azure resource. For example: https://westus.api.cognitive.microsoft.com
-        private static readonly string Endpoint = "CONTENT_MODERATOR_ENDPOINT";
-
+        // Name of the file that contains text
+        private static readonly string TextFile = "TextFile.txt";
+        // The name of the file to contain the output from the evaluation.
+        private static string TextOutputFile = "TextModerationOutput.txt";
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a text review client
+            ContentModeratorClient clientText = Authenticate(Secrets.SubscriptionKey, Secrets.Endpoint);        
+           
+            // Moderate text from text in a file
+            ModerateText(clientText, TextFile, TextOutputFile);
+        }
+
+        public static ContentModeratorClient Authenticate(string key, string endpoint)
+        {
+            ContentModeratorClient client = new ContentModeratorClient(new ApiKeyServiceClientCredentials(key));
+            client.Endpoint = endpoint;
+
+            return client;
+        }
+
+        /*
+ * TEXT MODERATION
+ * This example moderates text from file.
+ */
+        public static void ModerateText(ContentModeratorClient client, string inputFile, string outputFile)
+        {
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("TEXT MODERATION");
+            Console.WriteLine();
+            // Load the input text.
+            string text = File.ReadAllText(inputFile);
+
+            // Remove carriage returns
+            text = text.Replace(Environment.NewLine, " ");
+            // Convert string to a byte[], then into a stream (for parameter in ScreenText()).
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            MemoryStream stream = new MemoryStream(textBytes);
+
+            Console.WriteLine("Screening {0}...", inputFile);
+            // Format text
+
+            // Save the moderation results to a file.
+            using (StreamWriter outputWriter = new StreamWriter(outputFile, false))
+            {
+                using (client)
+                {
+                    // Screen the input text: check for profanity, classify the text into three categories,
+                    // do autocorrect text, and check for personally identifying information (PII)
+                    outputWriter.WriteLine("Autocorrect typos, check for matching terms, PII, and classify.");
+
+                    // Moderate the text
+                    var screenResult = client.TextModeration.ScreenText("text/plain", stream, "eng", true, true, null, true);
+                    outputWriter.WriteLine(JsonConvert.SerializeObject(screenResult, Formatting.Indented));
+                }
+
+                outputWriter.Flush();
+                outputWriter.Close();
+            }
+
+            Console.WriteLine("Results written to {0}", outputFile);
+            Console.WriteLine();
         }
     }
 }
